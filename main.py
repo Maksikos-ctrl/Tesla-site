c98d5a4df8ee19176c570e83a02e9a26655d0024
Maksikos-ctrl <maksikos973@gmail.com>
2021-11-14 23:48:50 +0200

    have done fisrt part of site

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/modelX")
+def modelX():
+    return render_template("index.html")    
+
+@app.route("/model3")
+def model3():
+    return render_template("index.html")  
+
+
+@app.route("/roadster")
+def roadster():
+    return render_template("index.html")   
+
+
+@app.route("/modelX")
+def ModelY():
+    return render_template("index.html")    
+    
+@app.route("/shop")
+def slider():
+    return render_template("index.html")  
+
+  
+
 
 
 if __name__ == "__main__":
