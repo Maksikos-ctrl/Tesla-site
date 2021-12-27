cce0004b109622e95b550461187412ade902d534
Maksikos-ctr <maksikos973@gmail.com>
2021-12-27 23:22:01 +0200

    Start to work with Star Rating, Now I am jsut practising and then am planning to connect js with AJAX

--- static/js/main.js
+++ static/js/main.js
@@ -17,14 +17,67 @@
 /* 
                                PLAN
 ------------------------------ FRONT-END ----------------------------------------------------------------                
-1) Зробити макет сайту
-2) Зробити responsive під мобіли
+1) Зробити макет сайту +
+2) Зробити responsive під мобіли +
 2) Додати фічі
 3) Зробити інші сторінки машин
 4) Зробити магазин у вигляді слайдера
 ------------------------------  BACK-END ---------------------------------------------------------------- 
 1) Додати Fetch API та DB.JSON щоб все зберігалося
-2) На вкладку Увійти приєднати Google Oauth2 else: просто створити сторінку для входу/реестрації що відкриватиметься при вході
+2) Зробити "Зоряний рейтинг" с AJAX
+3) На вкладку Увійти приєднати Google Oauth2 else: просто створити сторінку для входу/реестрації що відкриватиметься при вході
 
 */
+
+
+/* Зоряний рейтинг */
+const ratings = document.querySelectorAll('.rating');
+    
+
+if (ratings.length > 0) {
+    startRatings();
+}
+
+function startRatings() {  // Головна функція
+    let ratingActive, ratingValue;
+
+    for (let main = 0; main < ratings.length; main++) { // Передивляемося усы рейтинги на сторінці
+        const rating = ratings[main];
+        startRating(rating); 
+    }
+
+    function startRating(rating) { // Ініціалізація якогось конкретного рейтингу
+
+        startRatingVariables(rating);
+
+        changeRatingActiveWidth();
+
+        if (rating.classList.contains('rating__change')) {
+            changeRating(rating);
+        }
+    }
+
+    function startRatingVariables(rating) { // Ініціалізація змінних
+        ratingActive = rating.querySelector('.rating__active');
+        ratingValue = rating.querySelector('.rating__value');
+    }
+
+    function changeRatingActiveWidth(main = ratingValue.innerHTML) {
+        const changeRatingActiveWidth = main / 0.05; // Тому що 5 зірок
+        ratingActive.style.width = `${changeRatingActiveWidth}%`; // Записуємо змінену ширину активного рейтингу і через інтерполіяцію виведемо + додаємо %
+
+    }
+
+    function changeRating(rating) { // Можливість вказувати оцінку
+        const ratingItems = rating.querySelectorAll('.rating__item');
+        for (let m = 0; m < ratingItems.length; m++) {
+            const ratingItem = ratingItems[m]; 
+            ratingItem.addEventListener("mouseenter", () => {
+                startRatingVariables(rating); // Оновлення змінних
+                changeRatingActiveWidth(ratingItem.value); // Оновлення активних зірочок
+            });
+        }
+    }
+}
+
 
