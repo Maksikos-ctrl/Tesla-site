9a430c2674237219b72191a3810f922097afaef1
Maksikos-ctr <maksikos973@gmail.com>
2022-01-25 18:15:18 +0200

    Start working with google oauth2.0

---
+++ db.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_database():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            "sqlite_db", detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db 
+
+def close_database(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+def init_database():
+    db.get_database()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command("init_database")
+@with_appcontext
+def init_database_command():
+    """Очіщуэмо теперішню інфу і створюємо нові таблиці"""
+    init_database()
+    click.echo("Initialized the database.")
+
+
+def init_app(app):
+    app.teardown_appcontext(close_database)
+    app.cli.add_command(init_database_command)
--- main.py
+++ main.py
@@ -1,4 +1,58 @@
-from flask import Flask, render_template, jsonify, request
+"""
+Short Info about Google Oauth2.0 and how it works:
+
+Table of information how it works:
+
+1) What is the Oauth2.0?
+
+Oauth2.0 allows user to give permission for using this app and use it on their behalf.
+
+2) What is the OIDC abbreviation? What does it mean? How it works?
+
+Open ID Connect(OIDC) is an extension to Oauth2.0 providing features for your convenience. 
+
+3) Google oauth2.0 flow plan:
+
+1. Register your app with Google -> Google will provide a client id, and client secret
+
+2. Your app will redirect users to an authorization URl to log in with Google account -> This happens on Google's server, the user credentials are not exposed
+
+3. Google shows the user a consent screen(in which will be contained your data, access code to your application);
+
+4. Google provides an auth code
+
+5. My app exchanges this code for a token 
+
+6. Your app includes the token with subsequent requests to Google
+
+4) What OIDC doing?
+
+1.Publishes a well known endpoint -> My app can retrieve config info about the provider. Similarly, retrieve  basic user info 
+"""
+import json
+import os
+import sqlite3
+
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+from db import init_database_command
+from user import User
+
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 app = Flask(__name__)
 
